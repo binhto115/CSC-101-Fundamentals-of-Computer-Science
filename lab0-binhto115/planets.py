904ea730ed11962814365ce2553a62cf5272b2dd
Binh To <113943258+binhto115@users.noreply.github.com>
2025-04-24 13:52:56 -0700

    Add files via upload

---
+++ lab0-binhto115/planets.py
@@ -0,0 +1,12 @@
+def main(num: int):
+    mercuryWeight = num * 51.68
+    jupiterWeight = num * 344.08
+    return "What do you weigh on Earth " + f"{num}" + "\n" + "On Mercury you would weigh + f"{mercuryWeight}" +
+
+   # pass  # TODO: write the code for the program.
+
+
+# NOTE: This means if the code is run as `python3 planets.py`, run the
+# main function.  If the code is merely imported, don't do anything.
+if __name__ == "__main__":
+    main()
